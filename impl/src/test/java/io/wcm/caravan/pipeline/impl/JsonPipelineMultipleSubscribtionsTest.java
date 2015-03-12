1fef3cce5f86d610ddd3b03d9ef0bd9f43d2ef9f
Steffen Sauder <ssauder@pro-vision.de>
2015-03-12 21:00:08 +0100

    added another unit-tests to investigate/verify of multiple subscribers to the same pipeline

--- impl/src/test/java/io/wcm/caravan/pipeline/impl/JsonPipelineMultipleSubscribtionsTest.java
+++ impl/src/test/java/io/wcm/caravan/pipeline/impl/JsonPipelineMultipleSubscribtionsTest.java
@@ -26,6 +26,8 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import io.wcm.caravan.io.http.request.CaravanHttpRequestBuilder;
+import io.wcm.caravan.io.http.response.CaravanHttpResponse;
 import io.wcm.caravan.pipeline.JsonPipeline;
 import io.wcm.caravan.pipeline.JsonPipelineAction;
 import io.wcm.caravan.pipeline.JsonPipelineOutput;
@@ -35,22 +37,28 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Observer;
+import rx.Subscriber;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JsonPipelineMultipleSubscribtionsTest extends AbstractJsonPipelineTest {
 
   @Mock
   protected JsonPipelineAction action;
+
 
   public JsonPipelineMultipleSubscribtionsTest() {
     super();
@@ -118,6 +126,40 @@ public class JsonPipelineMultipleSubscribtionsTest extends AbstractJsonPipelineT
     fristSubscriber.toBlocking().single();
     secondSubscriber.toBlocking().single();
     verify(action, times(2)).execute(any());
+  }
+
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testTwoStepPipelineActionCalls() {
+
+    final AtomicInteger subscribeCount = new AtomicInteger();
+
+    Observable<CaravanHttpResponse> sourceObservable = Observable.create(new OnSubscribe<CaravanHttpResponse>() {
+
+      @Override
+      public void call(Subscriber<? super CaravanHttpResponse> t1) {
+        subscribeCount.incrementAndGet();
+
+        // if we uncomment this, this method gets called three times which makes the test fail
+        // TODO: investigate why this is happening
+        //t1.onNext(getJsonResponse(200, "{}", 0));
+        //t1.onCompleted();
+      }
+    });
+
+    JsonPipeline firstStep = new JsonPipelineImpl(new CaravanHttpRequestBuilder().build(), sourceObservable, caching);
+    JsonPipeline nextStep = firstStep.applyAction(action);
+    when(action.execute(any())).thenReturn(firstStep.getOutput());
+
+    // TODO: check if both observers onNext are called with the pipeline output
+    Observer<JsonPipelineOutput> firstObserver = Mockito.mock(Observer.class);
+    Observer<JsonPipelineOutput> secondObserver = Mockito.mock(Observer.class);
+
+    firstStep.getOutput().subscribe(firstObserver);
+    nextStep.getOutput().subscribe(secondObserver);
+
+    assertEquals(2, subscribeCount.get());
   }
 
   @Test
