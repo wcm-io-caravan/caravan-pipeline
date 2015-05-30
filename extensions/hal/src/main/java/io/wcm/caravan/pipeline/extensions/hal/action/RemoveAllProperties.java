7eecb72fbfa4bbc8a431122bc78e1c843eb3153f
Steffen Sauder <ssauder@pro-vision.de>
2015-05-30 17:53:01 +0200

    added RemoveAllProperties action

---
+++ extensions/hal/src/main/java/io/wcm/caravan/pipeline/extensions/hal/action/RemoveAllProperties.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.caravan.pipeline.extensions.hal.action;
+
+import io.wcm.caravan.commons.hal.resource.HalResource;
+import io.wcm.caravan.commons.stream.Streams;
+import io.wcm.caravan.pipeline.JsonPipelineAction;
+import io.wcm.caravan.pipeline.JsonPipelineContext;
+import io.wcm.caravan.pipeline.JsonPipelineOutput;
+
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import rx.Observable;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Sets;
+
+/**
+ * Removes all properties for a HAL resource and its embedded resources. (use {@link #except(String...)} to specify properties you want to keep.
+ */
+public class RemoveAllProperties implements JsonPipelineAction {
+
+  private final Set<String> propertiesToKeep = Sets.newHashSet();
+
+  @Override
+  public String getId() {
+    return propertiesToKeep.isEmpty() ? "REMOVE-ALL-PROPERTIES" : "REMOVE-ALL-PROPERTIES(" + StringUtils.join(propertiesToKeep, '-') + ")";
+  }
+
+  @Override
+  public Observable<JsonPipelineOutput> execute(JsonPipelineOutput previousStepOutput, JsonPipelineContext pipelineContext) {
+
+    HalResource hal = new HalResource((ObjectNode)previousStepOutput.getPayload());
+    removePropertiesRecursive(hal);
+    return Observable.just(previousStepOutput);
+
+  }
+
+  /**
+   * @param properties name of properties that should be preserved
+   * @return This action
+   */
+  public RemoveAllProperties except(String... properties) {
+
+    Streams.of(properties).forEach(relation -> propertiesToKeep.add(relation));
+    return this;
+
+  }
+
+  private void removePropertiesRecursive(HalResource hal) {
+
+    // remove properties
+    Streams.of(hal.getStateFieldNames())
+        .filter(property -> !propertiesToKeep.contains(property))
+        .forEach(property -> hal.getModel().remove(property));
+
+    // check embedded resources
+    Streams.of(hal.getEmbedded().values())
+        .forEach(embedded -> removePropertiesRecursive(embedded));
+
+  }
+
+}
